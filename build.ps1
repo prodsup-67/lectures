ba003fb7e76f7ed64d34e639c92ded262befff61
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-11-17 06:15:35 +0700

    Update

--- build.ps1
+++ build.ps1
@@ -1,59 +1,7 @@
 # $folder = "public\lectures\T01 - Introduction"
-# $folder = "public\lectures\T03 - CSS 1"
-# $folder = "public\lectures\T04 - CSS 2"
-
-# $folder = "public\lectures\T05 - JavaScript"
 # npm run html $folder
 # npm run pdf $folder
 
-# $folder = "public\lectures\T06 - TypeScript"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T07 - DOM"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T08 - React"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T09 - React Basics"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T10 - Data Fetching"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T11 - Database"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T13 - Tensorflow"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T14 - Image Classification"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T15 - LLM"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T16 - Blockchain 101"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T17 - Smart Contract"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "public\lectures\T18 - SC Applications"
-# npm run html $folder
-# npm run pdf $folder
-
-$folder = "public\lectures\T19 - DApp"
+$folder = "public\lectures\T02 - Node-Red"
 npm run html $folder
 npm run pdf $folder
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import FileBrowser from "./components/FileBrowser";
 import useData from "./lib/useData";
 
-const youtubeURL =
-  "https://youtube.com/playlist?list=PLNGLpHQhvGrvT_2VomKn37hYMRnP5L65n&si=_ZX4mMVbP57-roWz";
+const youtubeURL_S006 =
+  "https://www.youtube.com/playlist?list=PLNGLpHQhvGrthL1FV0OZIUQw_yZpv8Rkv";
 function App() {
   useData();
   return (
     <div className="container mx-auto mt-4">
       <div className="flex flex-col gap-3 items-start lg:flex-row lg:items-center lg:justify-between">
-        <div className="font-extrabold text-transparent text-6xl lg:text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        <div className="font-extrabold text-transparent text-6xl lg:text-8xl bg-clip-text bg-gradient-to-r from-purple-500 to-teal-400">
           Prodsup 67
         </div>
         <a
-          href={youtubeURL}
+          href={youtubeURL_S006}
           target="_blank"
           className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
         >
-          Youtube VDO
+          VDO (Sec 006)
         </a>
       </div>
       <div className="mt-8">
