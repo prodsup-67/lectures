3c8cfc82309ee1c5a76206e582e28fd26063a5f3
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-11-19 05:51:19 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import FileBrowser from "./components/FileBrowser";
 import useData from "./lib/useData";
 
+const youtubeURL_S003 =
+  "https://www.youtube.com/playlist?list=PLNGLpHQhvGrvUJ4ddNRL8VjVbdUPdAjTQ";
 const youtubeURL_S006 =
   "https://www.youtube.com/playlist?list=PLNGLpHQhvGrthL1FV0OZIUQw_yZpv8Rkv";
 function App() {
@@ -11,13 +13,22 @@ function App() {
         <div className="font-extrabold text-transparent text-6xl lg:text-8xl bg-clip-text bg-gradient-to-r from-purple-500 to-teal-400">
           Prodsup 67
         </div>
-        <a
-          href={youtubeURL_S006}
-          target="_blank"
-          className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
-        >
-          VDO (Sec 006)
-        </a>
+        <div className="flex gap-2">
+          <a
+            href={youtubeURL_S003}
+            target="_blank"
+            className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
+          >
+            VDO (Sec 003)
+          </a>
+          <a
+            href={youtubeURL_S006}
+            target="_blank"
+            className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
+          >
+            VDO (Sec 006)
+          </a>
+        </div>
       </div>
       <div className="mt-8">
         <FileBrowser />
