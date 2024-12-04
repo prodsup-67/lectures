43b013e203316db9715c467ffc5acebb411cbd51
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-12-04 14:18:47 +0700

    Update

--- build.ps1
+++ build.ps1
@@ -26,6 +26,18 @@
 # npm run html $folder
 # npm run pdf $folder
 
-$folder = "public\lectures\T09 - Cloud Database"
+# $folder = "public\lectures\T09 - Cloud Database"
+# npm run html $folder
+# npm run pdf $folder
+
+# $folder = "public\lectures\T10 - Fake Sensor"
+# npm run html $folder
+# npm run pdf $folder
+
+$folder = "public\lectures\T20 - Context"
+npm run html $folder
+npm run pdf $folder
+
+$folder = "public\lectures\T21 - Project"
 npm run html $folder
 npm run pdf $folder
