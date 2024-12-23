9e9bd23d9104e8378e0dfea5b368168e41b4712c
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-12-23 15:01:12 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ const youtubeURL_S003 =
   "https://www.youtube.com/playlist?list=PLNGLpHQhvGrvUJ4ddNRL8VjVbdUPdAjTQ";
 const youtubeURL_S006 =
   "https://www.youtube.com/playlist?list=PLNGLpHQhvGrthL1FV0OZIUQw_yZpv8Rkv";
+const youtubeURL_S001 =
+  "https://youtube.com/playlist?list=PLNGLpHQhvGrtGIPj6MzHepMhwr8KuQ5VZ&si=gUOXeBr_a4Bjs-cw";
+const youtubeURL_S005 =
+  "https://youtube.com/playlist?list=PLNGLpHQhvGrvKecKTi5o0kjLaYbUOZIjF&si=_Egf8nVoaZOSKs0s";
 const yourtubeURL_project =
   "https://www.youtube.com/playlist?list=PLNGLpHQhvGrtqn6UYxUYXm7C0auAYrh_O";
 function App() {
@@ -16,6 +20,20 @@ function App() {
           Prodsup 67
         </div>
         <div className="flex gap-2">
+          <a
+            href={youtubeURL_S001}
+            target="_blank"
+            className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
+          >
+            VDO (Sec 001)
+          </a>
+          <a
+            href={youtubeURL_S005}
+            target="_blank"
+            className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
+          >
+            VDO (Sec 005)
+          </a>
           <a
             href={youtubeURL_S003}
             target="_blank"
